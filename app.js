c3343ee922a554e7fcb261f3960351224b4a89fe
Wael <waelazar@gmail.com>
2017-08-11 14:25:33 +0000

    modify the DATABASERURL 3

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require("body-parser"),
 
 var url = process.env.DATABASEURL || "mongodb://localhost/restful_blog_app";
 mongoose.connect(url);
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
